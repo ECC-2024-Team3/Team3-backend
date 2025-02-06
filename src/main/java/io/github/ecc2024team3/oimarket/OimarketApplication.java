461ebf564f516e436989aa0cfe2661743f59a79e
Susie <thecatver@gmail.com>
2025-02-07 01:41:06 +0900

    feat: 초기 파일 세팅

---
+++ src/main/java/io/github/ecc2024team3/oimarket/OimarketApplication.java
@@ -0,0 +1,13 @@
+package io.github.ecc2024team3.oimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OimarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OimarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ecc2024team3/oimarket/OimarketApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ecc2024team3.oimarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OimarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
