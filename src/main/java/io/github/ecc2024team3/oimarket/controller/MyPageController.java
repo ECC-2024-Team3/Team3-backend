3a9037aad3b4c9b2ec316d0a62c32abfc23c74c3
Susie <thecatver@gmail.com>
2025-02-26 14:23:38 +0900

    chore: 죽은 패키지 삭제

--- src/main/java/io/github/ecc2024team3/oimarket/controller/MyPageController.java
+++ src/main/java/io/github/ecc2024team3/oimarket/controller/MyPageController.java
@@ -8,7 +8,6 @@ import io.github.ecc2024team3.oimarket.service.MyPageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
